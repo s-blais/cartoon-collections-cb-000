3482b246eded1c451ddf40f418d36e3ff2a03a14
Stephen Blais <stephen@s-blais.com>
2019-08-21 20:32:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def long_planeteer_calls (long_calls)
   long_calls.any? {|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese (snacks)
+  snacks.include?("cheddar", "gouda", "camembert")
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
